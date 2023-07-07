3f9eea62438f02e3c366efb5fd830696325843d5
Bhavya Marya <bhavyamarya2002@gmail.com>
2023-07-07 11:13:52 +0530

    ready for deployment

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -23,7 +23,21 @@ const Register = () => {
             const storageRef = ref(storage, `${displayName + date}`);
 
             const uploadTask = uploadBytesResumable(storageRef, file);
-            uploadTask.on(
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                    }
+                },
                 (error) => {
                     setErr(true);
                 },
