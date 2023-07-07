e1bf168e15834665b5f9464fa9919b52ac3d2049
Bhavya Marya <bhavyamarya2002@gmail.com>
2023-07-07 11:18:34 +0530

    ready for deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,18 @@ import { signOut } from 'firebase/auth'
 import React, { useContext } from 'react'
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
 
     const { currentUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        signOut(auth);
+        navigate("/login");
+
+    }
     return (
         <div className='navbar'>
             <span className='title'>Chit-Chat</span>
